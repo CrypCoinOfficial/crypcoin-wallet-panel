c004b8c91e6c3625e345560ceed66e52bf1cddb5
Daniel <admin@crypcoin.io>
2025-07-18 09:57:42 -0300

    Fix postcss.config y compatibilidad con Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h2 style={{ color: "gold", textAlign: "center", marginTop: "60px" }}>CrypCoin Wallet React Panel Ready</h2>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
