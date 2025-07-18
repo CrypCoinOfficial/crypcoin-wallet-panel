4cb818cce5e822282e423eb40f6fc9fc882dc992
CrypCoin Official <admin@crypcoin.io>
2025-07-18 17:00:00 -0300

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <h2 style={{ color: "gold", textAlign: "center", marginTop: "60px" }}>CrypCoin Wallet React Panel Ready</h2>;
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
